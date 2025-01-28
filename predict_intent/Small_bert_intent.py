a1043919ed349dbe3af6483a1a08b3ec529f318d
Vasiliki Rentoula <129061937+CuteQuacky@users.noreply.github.com>
2025-01-28 13:32:11 +0200

    updates

---
+++ predict_intent/Small_bert_intent.py
@@ -0,0 +1,356 @@
+import os
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
+import joblib
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+import seaborn as sns
+from pylab import rcParams
+import numpy as np
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelBinarizer
+import joblib
+import matplotlib.pyplot as plt
+import warnings
+
+tf.get_logger().setLevel('ERROR')
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#ADFF02", "#8F00FF"]
+sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+rcParams['figure.figsize'] = 12, 8
+warnings.filterwarnings("ignore")
+
+datafolder="/home/george/Desktop/Intent_Recognition_old/Data/"
+
+trainfile=datafolder+"train_data.csv"
+testfile=datafolder+"test_data.csv"
+validfile=datafolder+"val_data.csv"
+
+traindf = pd.read_csv(trainfile)
+validdf = pd.read_csv(validfile)
+testdf = pd.read_csv(testfile)
+print("Data loaded")
+
+traindf.head()
+traindf.shape
+
+trainfeatures=traindf.copy()
+trainlabels=trainfeatures.pop("intent")
+
+trainlabels = trainlabels.astype(str)  # Convert all entries to strings
+trainfeatures=trainfeatures.values
+
+chart = sns.countplot(trainlabels, palette=HAPPY_COLORS_PALETTE)
+plt.title("Number of texts per intent")
+chart.set_xticklabels(chart.get_xticklabels(), rotation=30, horizontalalignment='right');
+
+"""Distribution of class-labels in training-data:"""
+
+binarizer=LabelBinarizer()
+trainlabels=binarizer.fit_transform(trainlabels.values)
+trainlabels.shape
+
+"""Preprocess test- and validation data in the same way as it has been done for training-data: """
+
+testfeatures=testdf.copy()
+testlabels=testfeatures.pop("intent")
+validfeatures=validdf.copy()
+validlabels=validfeatures.pop("intent")
+testfeatures=testfeatures.values
+validfeatures=validfeatures.values
+testlabels=binarizer.transform(testlabels.values)
+validlabels=binarizer.transform(validlabels.values)
+
+"""SMALL-BERT"""
+
+bert_model_name = 'small_bert/bert_en_uncased_L-8_H-512_A-8'
+map_name_to_handle = {
+    'bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3',
+    'bert_en_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_cased_L-12_H-768_A-12/3',
+    'bert_multi_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/3',
+    'small_bert/bert_en_uncased_L-2_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-2_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-2_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-2_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-4_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-4_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-4_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-4_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-6_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-6_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-6_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-6_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-8_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-8_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-8_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-8_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-10_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-10_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-10_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-10_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-12_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-12_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-12_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-768_A-12/1',
+    'albert_en_base':
+        'https://tfhub.dev/tensorflow/albert_en_base/2',
+    'electra_small':
+        'https://tfhub.dev/google/electra_small/2',
+    'electra_base':
+        'https://tfhub.dev/google/electra_base/2',
+    'experts_pubmed':
+        'https://tfhub.dev/google/experts/bert/pubmed/2',
+    'experts_wiki_books':
+        'https://tfhub.dev/google/experts/bert/wiki_books/2',
+    'talking-heads_base':
+        'https://tfhub.dev/tensorflow/talkheads_ggelu_bert_en_base/1',
+}
+
+map_model_to_preprocess = {
+    'bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'bert_en_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_cased_preprocess/2',
+    'small_bert/bert_en_uncased_L-2_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-2_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-2_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-2_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-4_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-4_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-4_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-4_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-6_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-6_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-6_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-6_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-8_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-8_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-8_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-8_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-10_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-10_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-10_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-10_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-12_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-12_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-12_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'small_bert/bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'bert_multi_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/2',
+    'albert_en_base':
+        'https://tfhub.dev/tensorflow/albert_en_preprocess/2',
+    'electra_small':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'electra_base':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'experts_pubmed':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'experts_wiki_books':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+    'talking-heads_base':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2',
+}
+
+tfhub_handle_encoder = map_name_to_handle[bert_model_name]
+tfhub_handle_preprocess = map_model_to_preprocess[bert_model_name]
+
+print(f'BERT model selected           : {tfhub_handle_encoder}')
+print(f'Preprocess model auto-selected: {tfhub_handle_preprocess}')
+
+"""The preprocessing model"""
+
+bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+
+trainfeatures = np.array(trainfeatures, dtype=str)
+validfeatures = np.array(validfeatures, dtype=str)
+
+trainfeatures = tf.convert_to_tensor(trainfeatures, dtype=tf.string)
+trainlabels = tf.convert_to_tensor(trainlabels, dtype=tf.float32)
+
+validfeatures = tf.convert_to_tensor(validfeatures, dtype=tf.string)
+validlabels = tf.convert_to_tensor(validlabels, dtype=tf.float32)
+
+trainfeatures[0]
+
+text_test = trainfeatures[0]
+text_preprocessed = bert_preprocess_model(text_test)
+
+print(f'Keys       : {list(text_preprocessed.keys())}')
+print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
+print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
+print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
+print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
+
+bert_model = hub.KerasLayer(tfhub_handle_encoder)
+bert_results = bert_model(text_preprocessed)
+
+print(f'Loaded BERT: {tfhub_handle_encoder}')
+print(f'Pooled Outputs Shape:{bert_results["pooled_output"].shape}')
+print(f'Pooled Outputs Values:{bert_results["pooled_output"][0, :12]}')
+print(f'Sequence Outputs Shape:{bert_results["sequence_output"].shape}')
+print(f'Sequence Outputs Values:{bert_results["sequence_output"][0, :12]}')
+
+def build_classifier_model():
+  text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+  preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+  encoder_inputs = preprocessing_layer(text_input)
+  encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+  outputs = encoder(encoder_inputs)
+  net = outputs['pooled_output']
+  net = tf.keras.layers.Dropout(0.1)(net)
+  net = tf.keras.layers.Dense(5, activation=None, name='classifier')(net)
+  return tf.keras.Model(text_input, net)
+
+classifier_model = build_classifier_model()
+bert_raw_result = classifier_model(tf.constant(trainfeatures[0]))
+print(tf.keras.activations.softmax(bert_raw_result))
+
+
+classifier_model.summary()
+
+"""Model training"""
+
+loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+metrics = tf.metrics.CategoricalAccuracy()
+epochs=3
+optimizer=tf.keras.optimizers.Adam(1e-5)
+classifier_model.compile(optimizer=optimizer,
+                         loss=loss,
+                         metrics=metrics)
+
+print(f'Training model with {tfhub_handle_encoder}')
+history = classifier_model.fit(x=trainfeatures,y=trainlabels,validation_data=(validfeatures,validlabels),batch_size=32,epochs=epochs)
+
+# Save the trained model for future inference
+classifier_model.save('/home/george/Desktop/Intent_Recognition_old/intent_recognition_model.h5')
+print('Model saved as intent_recognition_model.h5')
+
+joblib.dump(binarizer, '/home/george/Desktop/Intent_Recognition_old/abel_binarizer.pkl')
+print('Label binarizer saved as label_binarizer.pkl')
+
+"""Evaluate the model"""
+
+loss, accuracy = classifier_model.evaluate(testfeatures,testlabels)
+
+print(f'Loss: {loss}')
+print(f'Accuracy: {accuracy}') 
+
+# Calculate F1-Score
+y_true = np.argmax(testlabels, axis=1)
+y_pred_probs = classifier_model.predict(testfeatures)
+y_pred = np.argmax(y_pred_probs, axis=1)
+
+f1 = f1_score(y_true, y_pred, average='weighted')
+print(f'Weighted F1 Score: {f1}')
+
+history_dict = history.history
+print(history_dict.keys())
+
+acc = history_dict['categorical_accuracy']
+val_acc = history_dict['val_categorical_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+fig = plt.figure(figsize=(10, 8))
+fig.tight_layout()
+
+plt.subplot(2, 1, 1)
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.grid(True)
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.grid(True)
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+
+def print_my_examples(inputs, results):
+  result_for_printing = \
+    [f'input: {inputs[i]:<30} : estimated intent: {results[i]}'
+                         for i in range(len(inputs))]
+  print(*result_for_printing, sep='\n')
+  print()
+
+examples = [
+    'I WANT SECURITY',
+    'I want PRIVACY',
+    'Develop multi-layered reliability monitoring framework',
+    'Develop multi-layered safety monitoring framework',
+    '5 stars for machines like me'
+]
+
+results = tf.nn.softmax(classifier_model(tf.constant(examples)))
+
+binarizer.classes_
+
+intents=binarizer.inverse_transform(results.numpy())
+
+print_my_examples(examples, intents)
+
+def predict_intent(texts):
+    processed_texts = tf.constant(texts)
+    predictions = model(processed_texts)
+    results = tf.nn.softmax(predictions)
+
+    return results
--- predict_intent/predict_intent.py
+++ predict_intent/predict_intent.py
@@ -1,69 +1,63 @@
 import tensorflow as tf
-import tensorflow_text as text
 import tensorflow_hub as hub
+import tensorflow_text as text
 import joblib
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 
-
-app = Flask(__name__)
-
-# Enable CORS for your frontend at http://10.220.2.185:3000
-CORS(app, resources={r"/*": {"origins": "http://10.220.2.185:3000"}})
-
-
+# Load model and label binarizer
 with tf.device('/CPU:0'):
-    model = tf.keras.models.load_model('intent_recognition_model.h5', custom_objects={'KerasLayer': hub.KerasLayer})
-    binarizer = joblib.load('label_binarizer.pkl')
+    model = tf.keras.models.load_model('/home/george/Desktop/Intent_Recognition/intent_recognition_model.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+    label_binarizer = joblib.load('/home/george/Desktop/Intent_Recognition/label_binarizer.pkl')
 
-# Preprocess and predict functions
+# Preprocessing function for input text
 def preprocess_input(texts):
+    
+    if isinstance(texts, str): 
+        texts = [texts]
     return tf.convert_to_tensor(texts, dtype=tf.string)
 
+# Prediction function
 def predict_intent(texts, threshold=0.5):
 
     processed_texts = preprocess_input(texts)
-
     predictions = model(processed_texts)
-
     probabilities = tf.nn.softmax(predictions).numpy()
 
+    # Get all possible class labels
+    class_labels = label_binarizer.classes_.tolist()
+
     predicted_classes = []
+    probs_with_functions = []
 
     for prob in probabilities:
-        # Check if any class probability exceeds the threshold
-        max_prob = max(prob)
+        max_prob = max(prob)  # Get the maximum probability for the current input
         if max_prob < threshold:
+            # Assign "other" if no class probability exceeds the threshold
             predicted_classes.append("other")
         else:
-            # If above the threshold, get the corresponding intent
-            predicted_class = binarizer.classes_[prob.argmax()]
+            # Get the class with the highest probability
+            predicted_class = class_labels[prob.argmax()]
             predicted_classes.append(predicted_class)
+        
+        # Map probabilities to class labels
+        prob_with_function = dict(zip(class_labels, prob.tolist()))
+        probs_with_functions.append(prob_with_function)
 
-    return predicted_classes
+    return predicted_classes, probs_with_functions
 
-# Handle POST requests
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = request.get_json()
-        texts = data.get('texts')
-        if not texts:
-            return jsonify({'error': 'No texts provided'}), 400
+# Example usage
+if __name__ == "__main__":
+    while True:
+        # Ask the user for input
+        user_input = input("Enter a sentence (or type 'exit' to quit): ")
+        if user_input.lower() == "exit":
+            print("Exiting the program. Goodbye!")
+            break
 
-        predicted_intents = predict_intent(texts)
-        return jsonify({'texts': texts, 'predicted_intents': predicted_intents})
+        # Get predictions and probabilities
+        intents, probabilities = predict_intent(user_input, threshold=0.6)
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-# Handle CORS preflight requests
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://10.220.2.185:3000')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-    return response
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        # Display the results
+        print(f"Input: {user_input}")
+        print(f"Predicted Intent: {intents[0]}")
+        print(f"Class Probabilities: {probabilities[0]}")
+        print("-" * 50)
